1248b59fa3281f3f380b63e6d0d2d2316149f8ca
Serge Zirukin <ftrvxmtrx@gmail.com>
2012-09-12 01:33:15 +0200

    allow extra arguments in build_run.sh

--- build_run.sh
+++ build_run.sh
@@ -2,6 +2,7 @@
 
 cabal build && ./dist/build/qvmgen/qvmgen -i ~/work/metaquake/src/game/quake/vm_builtins.c   -o builtins.txt   \
                                           -i ~/work/metaquake/src/game/quake/vm_extensions.c -o extensions.txt \
+                                          $@ \
                                           -- -DMETAQUAKE_CODE_GENERATION             \
                                              -I/home/ftrvxmtrx/work/metaquake/build/ \
                                              -I/home/ftrvxmtrx/work/metaquake/src    \
